f0add925dc40c26d870a5652e84735458c3a8a0c
DesmondM <desmondmpofu@gmail.com>
2019-01-04 11:03:00 +0200

    Add files via upload
    Enhanced Java loop

---
+++ JEnhancedLoop.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jenhancedloop;
+
+/**
+ *
+ * @author User
+ */
+public class JEnhancedLoop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a[]={10,20,30,40, 50};
+        
+        for(int x:a){
+            System.out.print(x + " ");
+        }
+    }
+    
+}
